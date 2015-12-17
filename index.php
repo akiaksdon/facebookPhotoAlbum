0c9b1b48355ce461fbb4e21e4fdbb2629289ad14
akiaksdon <akshay.d.waghmare@gmail.com>
2015-12-17 23:23:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,24 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
-   <?php
-require_once __DIR__ . '/vendor/autoload.php'
-
-session_start()
-
-$fb = new Facebook\Facebook([
-  'app_id' => '{1666988080247658}',
-  'app_secret' => '{a9fdc5c7ee7ea999755a91835e3279ca}',
-  'default_graph_version' => 'v2.5',
-  ]);
-
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://phpstack-14183-30913-76146.cloudwaysapps.com/login-callback.php', $permissions);
-
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-
-?>
+ 
  <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
