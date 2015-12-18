c2b792c001befa2bf560da65eb1b97d584b1422d
akshay w <akshay.d.waghmare@gmail.com>
2015-12-18 23:38:15 +0530

    changes

--- login-callback.php
+++ login-callback.php
@@ -49,19 +49,4 @@ $userName = $userNode->getName();
 $userId = $userNode->getId();
 echo 'https://graph.facebook.com/'.$user_id.'/picture';
 echo $userId;
-
-try {
-  $response = $fb->get('/'.$userId.'/albums');
-  $graphObject = $response->getGraphObject()->asArray();
-  } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-print_r($graphObject);
 ?>
