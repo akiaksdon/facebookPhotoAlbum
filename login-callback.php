18c6d0cf154b3c26a307f159addf17b10f2757e5
akshay w <akshay.d.waghmare@gmail.com>
2015-12-18 22:37:44 +0530

    changes

--- login-callback.php
+++ login-callback.php
@@ -48,7 +48,7 @@ echo 'Logged in as ' . $userNode->getName();
 
 try {
   $response = $fb->get('/me/albums');
-  $graphObject = $response->getGraphObject();
+  $graphEdge = $response->getGraphEdge();
   } catch(Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
@@ -59,5 +59,5 @@ try {
   exit;
 }
 
-print_r($graphObject);
+print_r($graphEdge);
 ?>
