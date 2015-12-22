22047cb525ffe240e9eeb0ba85fa7f2f32d392e4
akshay w <akshay.d.waghmare@gmail.com>
2015-12-22 14:06:52 +0530

    some new changes

--- login-callback.php
+++ login-callback.php
@@ -112,6 +112,76 @@ $userId = $userNode['id'];
             </div>
         </div> 
   </div>
+  <div class="row">
+        <ul class="thumbnails">
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                         <h3>Header Name</h3>
+                        <p>Description</p>
+                        <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </ul>
+    </div>
 </div>
 
 <?php
