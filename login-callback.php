aff9c4de74f0d82e744d77677d2e00e7159c169c
akshay w <akshay.d.waghmare@gmail.com>
2015-12-22 18:10:49 +0530

    some new changes

--- login-callback.php
+++ login-callback.php
@@ -209,7 +209,7 @@ foreach ($albumEdge as $album) {
     echo '<br />'.$album['id'].'  '.$album['name'];
 
     ?>
-    <img class="img-circle" src="<?php echo 'https://graph.facebook.com/'.$album['id'].'/picture';?>" />
+    <img class="img-circle" src="<?php echo 'https://graph.facebook.com/'.$album['id'].'/picture?type=album';?>" />
   <?php
   }
 
